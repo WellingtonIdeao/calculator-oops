e1abb2e20ed9e51f7457ea4c352cd6e59f6e6740
Wellington Ideão <wellington.ideao@gmail.com>
2025-05-29 18:52:22 -0300

    first commit

---
+++ src/main/java/br/com/ideao/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package br.com.ideao.calculator;
+
+/**
+ * Hello world!
+ */
+public final class Calculator {
+
+    private Calculator() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/br/com/ideao/calculator/util/ReadInput.java
@@ -0,0 +1,10 @@
+package br.com.ideao.calculator.util;
+
+import java.util.Scanner;
+
+public class ReadInput {
+    public static String read() {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
